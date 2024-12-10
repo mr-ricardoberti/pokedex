2c4db3c00c04f92f815c12f0c614b328504279ae
bertimaiara <mr.ricardoberti@gmail.com>
2024-12-10 20:47:28 -0300

    add mecanismo de paginação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+const limit = 10
+let offset = 0;
+
 function convertPokemonToLi(pokemon) {
   return `
       <li class="pokemon ${pokemon.type}">
@@ -16,8 +23,25 @@ function convertPokemonToLi(pokemon) {
   `
 }
 
-const pokemonList = document.getElementById("pokemonList");
+function loadPokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+      const newHtml = pokemons.map(convertPokemonToLi).join('')
+      pokemonList.innerHTML += newHtml
+  })
+}
 
-pokeApi.getPokemons().then((pokemons) => {
-  pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
-});
+loadPokemonItens(offset, limit)
+
+loadMoreButton.addEventListener('click', () => {
+  offset += limit
+  const qtdRecordsWithNexPage = offset + limit
+
+  if (qtdRecordsWithNexPage >= maxRecords) {
+      const newLimit = maxRecords - offset
+      loadPokemonItens(offset, newLimit)
+
+      loadMoreButton.parentElement.removeChild(loadMoreButton)
+  } else {
+      loadPokemonItens(offset, limit)
+  }
+})
